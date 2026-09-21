71a8877a7118599224d963964eae5d9bffd4234d
tpmej <ryserofficial@gmail.com>
2026-09-21 16:30:38 +0200

    Changes to be committed: 	deleted:    .vscode/launch.json 	new file:   LAB3/a.js 	new file:   LAB3/b.js 	new file:   LAB3/c.js 	new file:   LAB3/d.js 	new file:   LAB3/e.js 	new file:   LAB3/f.js 	new file:   LAB3/g.js#	new file:   LAB3/h.js 	new file:   LAB3/z36.js

---
+++ LAB3/a.js
@@ -0,0 +1,22 @@
+// Zadanie 1
+const przedmioty = ["Matematyka", "Język polski", "Język angielski", "Informatyka", "Fizyka"];
+console.log(przedmioty);
+console.log("Długość tablicy:", przedmioty.length);
+
+// Zadanie 2
+const kolory = ["czerwony", "zielony", "niebieski", "żółty"];
+console.log("Pierwszy:", kolory[0]);
+console.log("Trzeci:", kolory[2]);
+console.log("Ostatni:", kolory[kolory.length - 1]);
+
+// Zadanie 3
+const ceny = [10, 20, 30];
+ceny[1] = 25;
+console.log(ceny);
+
+// Zadanie 4
+const dowolnaTablica = [15, 42, 99, 108, 204];
+console.log("Ostatni element:", dowolnaTablica[dowolnaTablica.length - 1]);
+
+// Zadanie 5
+console.log([1, 2, 3][10]);
